53ca4d9c30ad9eed2a45c91d1729e0d931f44e17
mtosity <mtosity@gmail.com>
2024-09-03 00:25:36 -0400

    feat: register user route

---
+++ service/user/routes.go
@@ -0,0 +1,82 @@
+package user
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+
+	"github.com/go-playground/validator"
+	"github.com/gorilla/mux"
+	"github.com/mtosity-lab/backend/service/auth"
+	"github.com/mtosity-lab/backend/types"
+	"github.com/mtosity-lab/backend/utils"
+)
+
+type Handler struct {
+	store types.UserStore
+}
+
+func NewHandler(store types.UserStore) *Handler {
+	return &Handler{store: store}
+}
+
+func (h *Handler) RegisterRoute(router *mux.Router) {
+	router.HandleFunc("/login", h.handleLogin).Methods("POST")
+	router.HandleFunc("/register", h.handleRegister).Methods("POST")
+	router.HandleFunc("/mock-user", h.handleGetMockUser).Methods("GET")
+}
+
+func (h *Handler) handleLogin(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func (h *Handler) handleRegister(w http.ResponseWriter, r *http.Request) {
+	// get JSON payload
+	var payload types.RegisterUserPayload
+	if err := utils.ParseJSON(r, &payload); err != nil {
+		utils.WriteError(w, http.StatusBadRequest, err)
+		return
+	}
+
+	// validate the payload
+	if err := utils.Validate.Struct(payload); err != nil {
+		errors := err.(validator.ValidationErrors)
+		utils.WriteError(w, http.StatusBadRequest, fmt.Errorf("invalid payload: %s", errors))
+		return
+	}
+
+	// check if the user exists
+	_, err := h.store.GetUserByEmail(payload.Email)
+	if err != nil {
+		utils.WriteError(w, http.StatusConflict, fmt.Errorf("user with email %s already exists", payload.Email))
+		return
+	}
+
+	hashedPassword, err := auth.HashPassword(payload.Password)
+	if err != nil {
+		utils.WriteError(w, http.StatusInternalServerError, err)
+		return
+	}
+
+	// if it doesn't exist, create a new user
+	err = h.store.CreateUser(types.User{
+		FirstName: payload.FirstName,
+		LastName:  payload.LastName,
+		Email:     payload.Email,
+		Password:  hashedPassword,
+	})
+	if err != nil {
+		utils.WriteError(w, http.StatusBadRequest, err)
+		return
+	}
+
+	utils.WriteJSON(w, http.StatusCreated, nil)
+}
+
+func (h *Handler) handleGetMockUser(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(map[string]interface{}{
+		"username": "mtosity",
+		"email":    "mtosity@gmail.com",
+	})
+}
---
+++ service/user/routes_test.go
@@ -0,0 +1,89 @@
+package user
+
+import (
+	"bytes"
+	"encoding/json"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/gorilla/mux"
+	"github.com/mtosity-lab/backend/types"
+)
+
+func TestUser(t *testing.T) {
+	userStore := &mockUserStore{}
+	handler := NewHandler(userStore)
+
+	t.Run(("should fail if the user pauload is invalid"), func(t *testing.T) {
+		payload := types.RegisterUserPayload{
+			FirstName: "John",
+			LastName:  "Doe",
+			Email:     "asd",
+			Password:  "password",
+		}
+		marshalled, err := json.Marshal(payload)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		req, err := http.NewRequest(http.MethodPost, "/register", bytes.NewBuffer(marshalled))
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		rr := httptest.NewRecorder()
+		router := mux.NewRouter()
+
+		router.HandleFunc("/register", handler.handleRegister)
+		router.ServeHTTP(rr, req)
+
+		if rr.Code != http.StatusBadRequest {
+			t.Errorf("expected status code %d, got %d", http.StatusBadGateway, rr.Code)
+		}
+	})
+
+	t.Run("should create a new user", func(t *testing.T) {
+		payload := types.RegisterUserPayload{
+			FirstName: "John",
+			LastName:  "Doe",
+			Email:     "ad@gmail.com",
+			Password:  "password",
+		}
+
+		marshalled, err := json.Marshal(payload)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		req, err := http.NewRequest(http.MethodPost, "/register", bytes.NewBuffer(marshalled))
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		rr := httptest.NewRecorder()
+		router := mux.NewRouter()
+
+		router.HandleFunc("/register", handler.handleRegister)
+		router.ServeHTTP(rr, req)
+
+		if rr.Code != http.StatusCreated {
+			t.Errorf("expected status code %d, got %d", http.StatusCreated, rr.Code)
+		}
+	})
+
+}
+
+type mockUserStore struct{}
+
+func (m *mockUserStore) GetUserByEmail(email string) (*types.User, error) {
+	return nil, nil
+}
+
+func (m *mockUserStore) GetUserByID(id int) (*types.User, error) {
+	return nil, nil
+}
+
+func (m *mockUserStore) CreateUser(user types.User) error {
+	return nil
+}
